81447babea845e6226097e04032169b151451861
Masa <masa373.dev@gmail.com>
2020-01-08 01:22:35 +0800

    implement footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,29 @@
 import React from "react"
+import { Link } from "gatsby"
+import logo from '../images/logo-footer.jpg'
 
 const Footer = () => (
-  <footer>
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <a href="https://www.gatsbyjs.org">Gatsby</a>
+  <footer id="footer">
+    <div className="footer-top">
+      <ul className="footer-menu">
+        <li><Link to="/gallery">建築事例</Link></li>
+        <li><Link to="/recruit">リクルート</Link></li>
+        <li><Link to="/company">会社情報</Link></li>
+        <li><Link to="/contact">資料請求・お問い合わせ</Link></li>
+      </ul>
+      <div className="tel-container">
+        <p className="text">お気軽にご連絡ください</p>
+        <p className="tel">0797−84−5732</p>
+      </div>
+    </div>
+
+    <div className="footer-bottom">
+      <div className="footer-logo">
+        <Link to="/">
+          <img src={logo} alt="WOODONEKANSAI" />
+        </Link>
+      </div>
+    </div>
   </footer>
 )
 
