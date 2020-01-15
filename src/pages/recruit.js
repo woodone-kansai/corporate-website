762e46c922cdc6def5a6e73ebd1ec15e3102c8f4
Masa <masa373.dev@gmail.com>
2020-01-15 07:51:28 +0700

    create recruit page

---
+++ src/pages/recruit.js
@@ -0,0 +1,90 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const RecruitPage = ({ data }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
+  )
+  const metaDescription = site.siteMetadata.description
+
+  return (
+    <Layout>
+      <SEO title="求人情報" description={metaDescription} />
+      <div className="recruit">
+        <div className="heading-recruit-container">
+          <h2 className="heading-recruit en">RECRUIT</h2>
+          <hr />
+          <h2 className="heading-recruit ja">求人情報</h2>
+        </div>
+
+        <p className="description">
+          募集要項
+        </p>
+
+        <table>
+          <tr>
+            <th>勤務時間</th>
+            <td>9：00～18：00</td>
+            <td></td>
+          </tr>
+          <tr>
+            <th>休暇</th>
+            <td>※施工管理</td>
+            <td>日曜・水曜</td>
+          </tr>
+          <tr>
+            <th></th>
+            <td>※設計（コーディネーター含</td>
+            <td>日曜・水曜若しくは、火曜日・水曜日</td>
+          </tr>
+          <tr>
+            <th></th>
+            <td>※住宅営業</td>
+            <td>火曜・水曜</td>
+          </tr>
+          <tr>
+            <th></th>
+            <td>※法人営業C9</td>
+            <td>土曜・日曜</td>
+          </tr>
+          <tr>
+            <th></th>
+            <td colspan="2">年末年始休暇・夏季休暇・有給休暇（初年度25日※入社付により若干変動有）</td>
+          </tr>
+          <tr>
+            <th></th>
+            <td colspan="2">慶弔休暇、特別休暇（整理・出産・公傷・交通遮断）等</td>
+          </tr>
+          <tr>
+            <th>福利厚生</th>
+            <td colspan="2">保険：健康・厚生年金・雇用</td>
+          </tr>
+          <tr>
+            <th>お問合せ</th>
+            <td>株式会社ウッドワン関西</td>
+            <td></td>
+          </tr>
+          <tr>
+            <th></th>
+            <td>info@woodone-kansai.net</td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+    </Layout>
+  )
+}
+
+export default RecruitPage
