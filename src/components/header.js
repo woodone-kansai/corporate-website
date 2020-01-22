7312273afc9590b082ea4e4c6ad095d533fceb2d
Masa <masa373.dev@gmail.com>
2020-01-22 18:49:32 +0900

    use styled jsx in header, remove postcss

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { useStaticQuery, Link } from 'gatsby'
 
 const Header = () => {
@@ -14,19 +14,62 @@ const Header = () => {
   const logo = data.contentfulAsset
 
   return(
-    <header id="header">
-      <div className="header-logo">
-        <Link to="/">
-          <img src={logo.file.url} alt="WOODONEKANSAI" />
-        </Link>
-      </div>
-      <ul className="header-menu pc">
-        <li><Link to="/gallery/">建築事例</Link></li>
-        <li><Link to="/recruit/">リクルート</Link></li>
-        <li><Link to="/about/">会社情報</Link></li>
-        <li><Link to="/contact/">資料請求・お問い合わせ</Link></li>
-      </ul>
-    </header>
+    <Fragment>
+      <header id="header">
+        <div className="header-logo">
+          <Link to="/">
+            <img src={logo.file.url} alt="WOODONEKANSAI" />
+          </Link>
+        </div>
+        <ul className="header-menu pc">
+          <li><Link to="/gallery/">建築事例</Link></li>
+          <li><Link to="/recruit/">リクルート</Link></li>
+          <li><Link to="/about/">会社情報</Link></li>
+          <li><Link to="/contact/">資料請求・お問い合わせ</Link></li>
+        </ul>
+      </header>
+      <style jsx global>{`
+        header#header {
+          max-width: 960px;
+          height: 90px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin: 0 auto;
+          padding: 0 10px;
+          position: fixed;
+          right: 0;
+          left: 0;
+          top: 0;
+          z-index: 2;
+          background-color: #ffffff;
+          .header-logo {
+            width: 270px;
+            @media (max-width: 992px) {
+              width: 220px;
+            }
+            img {
+              width: 100%;
+            }
+          }
+          .header-menu {
+            display: flex;
+            li {
+              padding: 0 20px;
+              border-right: 1px solid #3a3a3a;
+              &:last-child {
+                border: none;
+                font-weight: bold;
+              }
+              a {
+                font-size: 15px;
+                color: #3a3a3a;
+              }
+            }
+          }
+        }
+      `}</style>
+    </Fragment>
   )
 }
 
